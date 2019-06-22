37dcce4a1264c80e411a6a893e903fee1632fe83
CaptainPica <44632287+CaptainPica@users.noreply.github.com>
2019-06-21 20:49:15 -0500

    Added alternating layout for page on individual states. Added charts and placeholder text

--- static/js/indiv.js
+++ static/js/indiv.js
@@ -1,27 +1,11 @@
 // .text() doesn't return the text. Weird.
 // Gets input state
 var text = d3.select("#inputin").node().innerHTML
-// Gets data to make graphs
-// Creates chart size
-svgWidth = 600;
-svgHeight = 400;
-var chartMargin = {
-    top: 30,
-    right: 30,
-    bottom: 30,
-    left: 30
-};
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-
-
 
 createChart(text,ageKeys,ageNames);
-createChart(text,sexKeys,sexNames);
+createChart(text,sexKeys,sexNames,false);
 createChart(text,incomeKeys,incomeNames);
-createChart(text,raceKeys,raceNames);
+createChart(text,raceKeys,raceNames,false);
 createChart(text,educationKeys,educationNames);
-createChart(text,exportKeys,exportNames);
+createChart(text,exportKeys,exportNames,false);
 createChart(text,politicalKeys,politicalNames);
