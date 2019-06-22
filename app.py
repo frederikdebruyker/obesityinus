2a7aab21d284c3e55056c423ac7b27ffc39f40f6
techsmj <joyetuks@gmail.com>
2019-06-21 23:46:18 -0500

    update sankey graph

--- app.py
+++ app.py
@@ -106,11 +106,16 @@ def create_compilation(dic):
 
 def createSankeyInput(dic_in,obesity_keys,count_keys,names):
     mults = []
-    outJSON = {"nodes":[{"node":0,"name":"Obesity"}],"links":[]}
+    outJSON = []
+    
     for i in range(len(names)):
+    #      if i = "not_significant"
+              
+    #        else {
+    #            values.push(0.0)
         mults.append(float(dic_in[obesity_keys[i]])*float(dic_in[count_keys[i]])/100)
-        outJSON["nodes"].append({"node":(i+1),"name":names[i]})
-        outJSON["links"].append({"source":0,"target":(i+1),"value":mults[i]})
+        # outJSON["nodes"].append({"node":(i+1),"name":names[i]})
+        outJSON.append({"from":"Obesity","to":names[i],"weight":mults[i]})
     return outJSON
 
 
