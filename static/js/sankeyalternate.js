0fc3e6af63f135a8efb924ac291560e3bc4bc84c
frederikdebruyker <frederik@debruyker.com>
2019-06-21 21:28:53 -0500

    sankey alternate

--- static/js/sankeyalternate.js
+++ static/js/sankeyalternate.js
@@ -1,17 +1,43 @@
-var data = [
-    { from: "Obesity", to: "Below 17K", weight: 20000 },
-    { from: "Obesity", to: "17-25K", weight: 17000 },
-    { from: "Obesity", to: "25K-35K", weight: 8000 },
-    { from: "Obesity", to: "35-45K", weight: 11000 },
-    { from: "Obesity", to: "45-60K", weight: 7500 },
-    { from: "Obesity", to: "60-75K", weight: 5000 },
-    { from: "Obesity", to: "Over 75K", weight: 4000 }
-];
-
-var sankey_chart = anychart.sankey(data);
-
-sankey_chart.nodeWidth("100%");
-sankey_chart.nodePadding(20000);
-
-sankey_chart.container("SankeyContainer");
-sankey_chart.draw();
+// var data = [
+    var data = [
+        {
+          "from": "Obesity",
+          "to": "Less than 15k",
+          "weight": 460.845
+        },
+        {
+          "from": "Obesity",
+          "to": "15k-25k",
+          "weight": 658.44
+        },
+        {
+          "from": "Obesity",
+          "to": "25k-35k",
+          "weight": 385.53
+        },
+        {
+          "from": "Obesity",
+          "to": "35k-50k",
+          "weight": 371.52
+        },
+        {
+          "from": "Obesity",
+          "to": "50k-75k",
+          "weight": 488.6
+        },
+        {
+          "from": "Obesity",
+          "to": "Greater than 75k",
+          "weight": 896.5440000000001
+        }
+       ];
+    
+      
+    
+    var sankey_chart = anychart.sankey(data);
+    
+    sankey_chart.nodeWidth("20%");
+    sankey_chart.nodePadding(20);
+    
+    sankey_chart.container("SankeyContainer");
+    sankey_chart.draw();
