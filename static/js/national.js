75b7c08a985e4a89632a3b247a2604224e638d84
Tolani Afolabi <tolaniafolabi24@gmail.com>
2019-06-22 00:11:14 -0500

    uploading the complete politican and religious bar charts

---
+++ static/js/national.js
@@ -0,0 +1,6 @@
+var stateCharts ="National";
+createChart(stateCharts,ageKeys,ageNames);
+createChart(stateCharts,sexKeys,sexNames,false);
+createChart(stateCharts,incomeKeys,incomeNames);
+createChart(stateCharts,raceKeys,raceNames,false);
+createChart(stateCharts,educationKeys,educationNames);
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,13 +1,14 @@
-var svgWidth = 960;
-var svgHeight = 640;
+svgWidth = 960;
+svgHeight = 640;
 
 var chartMargin = {
   top: 20,
   right: 60,
   bottom: 300,
-  left: 100
+  left: 100,
 };
-
+var politicalKeys = ["democrat","republican"];
+var politicalNames = ["Democrat","Republican"];
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
@@ -31,13 +32,14 @@ d3.json("/happinessData", function(err, happinessData) {
 });
 
 
+
 function createAggBar(names, values) {
   // Append svg and set dimensions
   var svg = d3.select(".container-fluid")
   .append("div")
   .classed("row",true)
   .append('div')
-  .classed("col-lg-6 col-md-12",true)
+  .classed("col-md",true)
   .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth);
@@ -77,69 +79,83 @@ function createAggBar(names, values) {
       .attr("height", (chartHeight - yScale(point)));
   });
 };
+
+var religiousKeys =["non_religious","strongly_religious"];
+var religiousNames= ["Non-Religious","Religious"]
 
 d3.json("/happinessData", function(err, happinessData) {
-  if (err) throw err;
-  var count =0;
-  var relSum =0;
-  var nonrelSum =0;
-  happinessData.forEach(function(state) {
-    state.strongly_religious=+state.strongly_religious
-    state.non_religious=+state.non_religious
-    if (state.strongly_religious>state.non_religious){
-      nonrelSum = nonrelSum+state.non_religious;
-      count = count +1;
-    }
-    else {relSum = relSum+strongly_religious};
+    if (err) throw err;
+    var count =0;
+    var relSum =0;
+    var nonrelSum =0;
+    happinessData.forEach(function(state) {
+      state.strongly_religious=+state.strongly_religious;
+      state.non_religious=+state.non_religious;
+      if (state.strongly_religious>state.non_religious){
+        relSum = relSum+state.strongly_religious;
+        count = count +1;
+        console.log(state.strongly_religious);
+      }
+      else {nonrelSum = nonrelSum+state.non_religious};
+      console.log(state.non_religious);
+    });
+    nonrelSum = nonrelSum/count;
+    relSum = relSum/(50-count);
+    createRegBar(religiousNames,[relSum,nonrelSum]);
   });
-  nonrelSum = nonrelSum/count;
-  relSum = relSum/(50-count);
-  createRegBar(religiousNames,[relSum,nonrelSum]);
-});
-
-function createRegBar(names, values) {
-  // Append svg and set dimensions
-  var svg = d3.select(".container-fluid")
-  .append("div")
-  .classed("row",true)
-  .append('div')
-  .classed("col-lg-6 col-md-12",true)
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
-  // Add location actual chart will be on
-  var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-  // Move all below to a function
-  var xScale = d3.scaleBand()
-      .domain(names)
-      .range([0,chartWidth])
-      .padding(.1);
-
-  var yScale = d3.scaleLinear()
-      .domain([d3.min(values)-5,d3.max(values)])
-      .range([chartHeight,0]);
-
-  var xAxis = d3.axisBottom(xScale);
-  var yAxis = d3.axisLeft(yScale);
-
-  // Append two SVG group elements to the chartGroup area,
-  // and create the bottom and left axes inside of them
-  chartGroup.append("g")
-  .call(yAxis);
-
-  chartGroup.append("g")
-  .attr("transform", `translate(0, ${chartHeight})`)
-  .call(xAxis);
-
-  // Create one SVG rectangle per piece of Data
-  // Use the linear and band scales to position each rectangle within the chart
-  values.forEach((point,index)=>{
-      chartGroup.append("rect")
-      .attr("class", "bar")
-      .attr("x", xScale(names[index]))
-      .attr("y", yScale(point))
-      .attr("width", xScale.bandwidth())
-      .attr("height", (chartHeight - yScale(point)));
-  });
-};
+  
+  function createRegBar(names, values) {
+    // Append svg and set dimensions
+    var svg = d3.select(".row")
+    .append("div")
+    .classed("row",true)
+    .append('div')
+    .classed("col-md",true)
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+    // Add location actual chart will be on
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+    // Move all below to a function
+    var xScale = d3.scaleBand()
+        .domain(names)
+        .range([0,chartWidth])
+        .padding(.1);
+  
+    var yScale = d3.scaleLinear()
+        .domain([d3.min(values)-5,d3.max(values)])
+        .range([chartHeight,0]);
+  
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+  
+    // Append two SVG group elements to the chartGroup area,
+    // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+    .call(yAxis);
+  
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(xAxis);
+  
+    // Create one SVG rectangle per piece of Data
+    // Use the linear and band scales to position each rectangle within the chart
+    values.forEach((point,index)=>{
+        chartGroup.append("rect")
+        .attr("class", "bar")
+        .attr("x", xScale(names[index]))
+        .attr("y", yScale(point))
+        .attr("width", xScale.bandwidth())
+        .attr("height", (chartHeight - yScale(point)));
+        
+    });
+   
+  };
+              
+  var stateCharts ="National";
+createChart(stateCharts,ageKeys,ageNames);
+createChart(stateCharts,sexKeys,sexNames,false);
+createChart(stateCharts,incomeKeys,incomeNames);
+createChart(stateCharts,raceKeys,raceNames,false);
+createChart(stateCharts,educationKeys,educationNames);
